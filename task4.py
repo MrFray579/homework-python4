ce5e330380db6d93bbc969d50bf1839436c122dc
Fray <kozar20000@mail.ru>
2022-09-21 22:13:00 +0300

    corrected

--- task4.py
+++ task4.py
@@ -4,8 +4,14 @@ k = int(input('k = '))
 arr = list()
 for i in range(k, 0, -1):
     x = rd(-100, 100)
-    if x > 0 and i != k:
-        arr.append(f'+{x}x^{i}')
-    else:
-        arr.append(f'{x}x^{i}')
+    if x != 0:
+        if x > 0 and i != k:
+            arr.append(f'+{x}x^{i}')
+        else:
+            arr.append(f'{x}x^{i}')
+x = rd(-100, 100)
+if x > 0 and i != k:
+    arr.append(f'+{x}')
+else:
+    arr.append(f'{x}')
 print(''.join(arr),'= 0')
